a98526aee78b2ce18425bbb6b8cc509f0e6bfab2
Sasa Juratovic <sasa.juratovic@microsoft.com>
2024-08-12 11:12:49 +0200

    Refactor code structure and import statements in streamlit_app.py and helpers.py

--- src/frontend/streamlit_app.py
+++ src/frontend/streamlit_app.py
@@ -2,13 +2,9 @@ import os
 import time
 import json
 import streamlit as st
-#import sys
 from urllib.parse import urlparse
-#from dotenv import find_dotenv, load_dotenv
 from pytubefix import YouTube
 from pytubefix.cli import on_progress
-
-#sys.path.append('../')
 from helpers import *
 
 def main():
@@ -25,41 +21,49 @@ def main():
     # Language in the video for Azure Speech to Text
     language = "en-US"
 
-    # Azure OpenAI GPT deployed model 
-    # Defined in infra/main.bicep as '<model>-<version>'
-    # Shall be defined in .env file?
-    # model = "gpt-4o-2024-05-13"
-    model = "gpt-4o-mini"
+    # Azure AI model 
+    model = "gpt-4o-2024-05-13"
 
-    st.title("SOP Document Generator")
+    st.title("VANTAGE Genie Accelerator")
+    st.markdown("#### (Video Analysis, Notation, Transcription, and Generation Engine)")
 
     source_video_file = "https://raw.githubusercontent.com/retkowsky/samplesvideos/main/surgical_sop.mp4"
-    src_video_file = st.text_input("Download following video file:", source_video_file)
-
+    src_video_file = st.text_input("Process video file from:", source_video_file)
     dst_video_folder = st.text_input("Save video file to:", VIDEO_DIR)
 
-    if st.button("Download video file"):
-
-#        if not os.path.isfile(dst_video_file):
+    if st.button("Start processing"):
+        st.divider()
+        # Download YouTube video
         if "youtu" in src_video_file:
             youtube = YouTube(src_video_file, on_progress_callback = on_progress)
             video = youtube.streams.get_highest_resolution()
             dst_video_file = os.path.join(dst_video_folder, video.default_filename)
             if not os.path.isfile(dst_video_file):
+                st.info(f"Downloading {src_video_file}")
+                start = time.time()
                 video = download_youtube_video(src_video_file, dst_video_folder)
+                elapsed = time.time() - start
+                st.info("Completed in " + time.strftime(
+                    "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
+            else:
+                st.info(f"Using local copy because {src_video_file} has already been previously downloaded")
 
-            video_file = open(dst_video_file, "rb")
-            video_bytes = video_file.read()
-            st.video(video_bytes)
-
+        # Download video from direct link
         else:
             dst_video_file = os.path.join(dst_video_folder, os.path.basename(urlparse(src_video_file).path))
             if not os.path.isfile(dst_video_file):
+                st.info(f"Downloading {src_video_file}")
+                start = time.time()
                 download_file(src_video_file, dst_video_file)
+                elapsed = time.time() - start
+                st.info("Completed in " + time.strftime(
+                    "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
+            else:
+                st.info(f"Using local copy because {src_video_file} has already been previously downloaded")
 
-            video_file = open(dst_video_file, "rb")
-            video_bytes = video_file.read()
-            st.video(video_bytes)
+        video_file = open(dst_video_file, "rb")
+        video_bytes = video_file.read()
+        st.video(video_bytes)
 
         # Display video file information
         file_name, file_size_mb, formatted_time = display_file_info(video_file.name)
@@ -67,8 +71,6 @@ def main():
         hours = int(duration // 3600)
         minutes = int((duration % 3600) // 60)
         seconds = int(duration % 60)
-
-        st.divider()
         video_file_info = f"""
         ### Video File Information \n
         Source: <{src_video_file}>  \n
@@ -81,67 +83,43 @@ def main():
         Frames per second (FPS): {fps:.0f}
         """
         st.info(video_file_info)
-        # st.markdown("### Video File Information")
-        # st.markdown(f"File: {file_name}")
-        # st.markdown(f"Size: {file_size_mb:.2f} MB")
-        # st.markdown(f"Last Modified: {formatted_time}")
-        # st.markdown(f"Duration: {duration:.0f} seconds".format(duration))
-        # st.markdown(f"Length of video: {hours:02}:{minutes:02}:{seconds:02}")
-        # st.markdown(f"Number of frames: {total_frames}")
-        # st.markdown(f"Frames per second (FPS): {fps:.0f}")
-
-        # Display video file frames
-        # display_video_frames(video_file.name)
 
         # Extract audio from video file
         st.divider()
-        #st.markdown("### Audio file information")
-        st.info("Extracting audio from video file")
-        start = time.time()
-        audio_file = get_audio_file(video_file.name, RESULTS_DIR)
-        elapsed = time.time() - start
-        st.info("Completed in " + time.strftime(
-            "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
-        # Display audio file information
-        #file_name, file_size_mb, formatted_time = display_file_info(audio_file)
-        # st.markdown("### Audio File Information")
-        #st.markdown(f"File: {file_name}")
-        #st.markdown(f"Size: {file_size_mb:.2f} MB")
-        #st.markdown(f"Last Modified: {formatted_time}")
-        #st.markdown("#### Waveplot of audio file")
-        #display_amplitude(audio_file)
+        audio_file = os.path.join(
+            RESULTS_DIR,
+            os.path.splitext(os.path.basename(video_file.name))[0] + ".wav"
+            )
+        if not os.path.isfile(audio_file):
+            st.info("Extracting audio from video file")
+            start = time.time()
+            audio_file = get_audio_file(video_file.name, RESULTS_DIR)
+            elapsed = time.time() - start
+            st.info("Completed in " + time.strftime(
+                "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
+        else:
+            st.info(f"Using local copy because {audio_file} has already been extracted")
 
         # Transcribe audio file using Azure Speech to Text
         st.divider()
-        #st.markdown("### Extract text from audio file")
-        st.info("Transcribing audio file")
+        st.info("Transcribing audio file to text")
         start = time.time()
         transcript, confidence, words = azure_text_to_speech(audio_file, language)
         elapsed = time.time() - start
         st.info("Completed in " + time.strftime(
             "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
-        #transcript_text = ' '.join(transcript)
-        #st.markdown(f"Transcript text lenght: {len(transcript_text)}")
-        #st.markdown(f"Transcript: {transcript_text}")
-
-        #st.markdown("#### Text transcript with confidence level")
         df = pd.DataFrame(words, columns=["Word", "Offset", "Duration", "Confidence"])
         df["Offset_in_secs"] = df["Offset"] / 10_000_000
         df["Duration_in_secs"] = df["Duration"] / 10_000_000
-        #st.dataframe(df, use_container_width=True)
-
         transcript_file = os.path.join(
             RESULTS_DIR,
             os.path.splitext(os.path.basename(video_file.name))[0] + ".txt"
             )
-
         df.to_csv(transcript_file)
-        #st.info(f"Transcript saved to {transcript_file}")
 
-        # Create SOP structure using Azure OpenAI
+        # Create SOP document structure using Azure AI
         st.divider()
-        #st.markdown("### Create SOP structure using Azure OpenAI")
-        st.info("Processing transcribed text using LLM ...")
+        st.info("Creating SOP document structure")
         sop_text = df.to_json(orient="records")
         prompt = """
         Describe the main steps of this checklist document.
@@ -185,27 +163,22 @@ def main():
             ]
         }
         """
-
         start = time.time()
         completion = ask_gpt4o(prompt, sop_text, model)
         elapsed = time.time() - start
         st.info("Completed in " + time.strftime(
             "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
-        #st.markdown(f"Completion: {completion}")
-        #df = pd.DataFrame.from_dict(json.loads(completion)['steps'])
-        #st.dataframe(df, use_container_width=True)
 
-        # Create SOP in Microsoft Word format
+        # Create SOP document in Microsoft Word format
         st.divider()
-        #st.markdown("### Create SOP in Microsoft Word format")
-        st.info("Processing transcribed text and video ...")
+        st.info("Creating SOP document in Microsoft Word format")
         json_data = json.loads(completion)["Steps"]
         start = time.time()
         docx_file = checklist_docx_file(video_file.name, json_data, RESULTS_DIR, model, 1)
         elapsed = time.time() - start
         st.info("Completed in " + time.strftime(
             "%H:%M:%S.{}".format(str(elapsed % 1)[2:])[:15], time.gmtime(elapsed)))
-        st.info(f"SOP saved to {docx_file}")
+        st.info(f"SOP document saved to {docx_file}")
         
 
 if __name__ == "__main__":
